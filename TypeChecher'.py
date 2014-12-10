ab82c0a9b28e23de2529e910b046ef649f8314a1
unknown <ma.wilk@tlen.pl>
2014-12-10 14:21:45 +0100

    types

--- TypeChecher'.py
+++ TypeChecher'.py
@@ -3,14 +3,13 @@ import AST
 
 
 class NodeVisitor(object):
-
     def visit(self, node):
         method = 'visit_' + node.__class__.__name__
         visitor = getattr(self, method, self.generic_visit)
         return visitor(node)
 
 
-    def generic_visit(self, node):        # Called if no explicit visitor function exists for a node.
+    def generic_visit(self, node):  # Called if no explicit visitor function exists for a node.
         if isinstance(node, list):
             for elem in node:
                 self.visit(elem)
@@ -23,38 +22,36 @@ class NodeVisitor(object):
                 elif isinstance(child, AST.Node):
                     self.visit(child)
 
-    # simpler version of generic_visit, not so general
-    #def generic_visit(self, node):
-    #    for child in node.children:
-    #        self.visit(child)
-
+                    # simpler version of generic_visit, not so general
+                    #def generic_visit(self, node):
+                    #    for child in node.children:
+                    #        self.visit(child)
 
 
 class TypeChecker(NodeVisitor):
-
     def visit_BinExpr(self, node):
-                                          # alternative usage,
-                                          # requires definition of accept method in class Node
-        type1 = self.visit(node.left)     # type1 = node.left.accept(self) 
-        type2 = self.visit(node.right)    # type2 = node.right.accept(self)
-        op    = node.op
+                                        # alternative usage,
+                                        # requires definition of accept method in class Node
+        type1 = self.visit(node.left)   # type1 = node.left.accept(self)
+        type2 = self.visit(node.right)  # type2 = node.right.accept(self)
+        op = node.op
         # ... 
         #
- 
+
     def visit_RelExpr(self, node):
-        type1 = self.visit(node.left)     # type1 = node.left.accept(self) 
-        type2 = self.visit(node.right)    # type2 = node.right.accept(self)
+        type1 = self.visit(node.left)   # type1 = node.left.accept(self)
+        type2 = self.visit(node.right)  # type2 = node.right.accept(self)
         # ... 
         #
 
     def visit_Integer(self, node):
         return 'int'
 
-    #def visit_Float(self, node):
-    # ... 
-    # 
+        #def visit_Float(self, node):
+        # ...
+        #
 
-    # ... 
-    # 
+        # ...
+        #
 
 
---
+++ Types.py
@@ -0,0 +1,33 @@
+bin_op = ['OR','AND','SHL','SHR']
+arith_op = ['*', '/', '+', '-']
+comp_op = [ '<', '>', 'EQ', 'NEQ', 'LE', 'GE']
+
+class Ttype(dict):
+
+    def __missing__(self, key):
+        self[key] = Ttype()
+        return self[key]
+
+ttype = Ttype()
+
+for op in bin_op+arith_op:
+    ttype[op]['int']['int'] = 'int'
+
+for op in arith_op:
+    ttype[op]['float']['float'] = 'float'
+
+for op in comp_op:
+    ttype[op]['int']['int'] = 'int'
+    ttype[op]['float']['float'] = 'int'
+    ttype[op]['string']['string'] = 'int'
+
+
+ttype['=']['int']['int'] = 'int'
+ttype['=']['float']['float'] = 'float'
+ttype['=']['string']['string'] = 'string'
+ttype['=']['int']['float'] = 'int'
+ttype['=']['float']['int'] = 'float'
+
+ttype['+']['string']['string'] = 'string'
+ttype['*']['string']['int'] = 'string'
+
